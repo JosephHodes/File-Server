44b291c455d34f6ddbcbcfbc47586529aed1d2ee
Joseph Hodes <jojoho70@gmail.com>
2021-11-11 00:41:12 -0500

    Figured out how tests are going to be laid out

--- Index.go
+++ Index.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Print("hello world")
+	switch strings.ToUpper(os.Getenv("Mode")){
+	case "TEST":
+		
+	}
+	log.Fatalln("fatal error couldn't get mode it was defined as: "+os.Getenv())
 }
