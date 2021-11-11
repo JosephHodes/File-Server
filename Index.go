566da1befc190b495c036ad73144a3c6f134aa67
Joseph Hodes <jojoho70@gmail.com>
2021-11-11 01:25:24 -0500

    Figured out how to format

--- Index.go
+++ Index.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"./Tests/FuncTests"
 	"log"
 	"os"
 	"strings"
@@ -9,7 +10,10 @@ import (
 func main() {
 	switch strings.ToUpper(os.Getenv("Mode")){
 	case "TEST":
-		
+		err := FuncTests.FuncTest()
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 	log.Fatalln("fatal error couldn't get mode it was defined as: "+os.Getenv())
 }
---
+++ Tests/FuncTests/FuncTester.go
@@ -0,0 +1,5 @@
+package FuncTests
+
+func FuncTest() error {
+return nil;
+}
